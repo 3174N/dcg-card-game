23cfe3a4ebd4fab3cfea1925b9a8ad9d38a1318f
Embersands game Studios <eitan.levy.159@gmail.com>
2020-10-14 10:01:06 +0300

    Init

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    #region Variables
+    
+    public enum Player
+    {
+        Player,
+        Enemy
+    }
+    public Player player;
+    
+    private GameObject dropZone;
+
+    private bool isDragging = false;
+    private bool isOverDropZone = false;
+
+    private Vector2 startPosition;
+
+    #endregion
+
+    private void Start()
+    {
+        switch (player)
+        {
+            case Player.Player:
+                dropZone = GameManager.instance.playerDropZone;
+                break;
+            case Player.Enemy:
+                dropZone = GameManager.instance.enemyDropZone;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (isDragging)
+        {
+            transform.position = Input.mousePosition;
+        }
+    }
+
+    public void StartDrag()
+    {
+        startPosition = transform.position;
+        isDragging = true;
+    }
+
+    public void EndDrag()
+    {
+        isDragging = false;
+
+        if (isOverDropZone)
+        {
+            transform.SetParent(dropZone.transform, false);
+        }
+        else
+        {
+            transform.position = startPosition;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject == dropZone)
+        {
+            isOverDropZone = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject == dropZone)
+        {
+            isOverDropZone = false;
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region Variables
+
+    public static GameManager instance;
+
+    public GameObject playerArea;
+    public GameObject playerDropZone;
+    public GameObject enemyArea;
+    public GameObject enemyDropZone;
+    
+    public List<GameObject> cards = new List<GameObject>();
+
+    #endregion
+
+    private void Awake()
+    {
+        if (instance != null)
+            Destroy(gameObject);
+        
+        DontDestroyOnLoad(gameObject);
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    public void DrawCards()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            GameObject playerCard = Instantiate(cards[Random.Range(0, cards.Count)], new Vector3(0f, 0f, 0f), Quaternion.identity);
+            playerCard.transform.SetParent(playerArea.transform, false);
+            playerCard.GetComponent<Card>().player = Card.Player.Player;
+        }
+        
+        for (int i = 0; i < 5; i++)
+        {
+            GameObject enemyCard = Instantiate(cards[Random.Range(0, cards.Count)], new Vector3(0f, 0f, 0f), Quaternion.identity);
+            enemyCard.transform.SetParent(enemyArea.transform, false);
+            enemyCard.GetComponent<Card>().player = Card.Player.Enemy;
+        }
+    }
+}
